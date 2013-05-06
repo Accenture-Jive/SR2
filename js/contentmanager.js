f85d4496fd4dcd68b3c16daf920e1127545ba4ac
Ripul Thakur <ripul.thakur@accenture.com>
2013-05-06 17:00:48 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -2186,7 +2186,8 @@ if (categg1[ind]==selected_cat)
 console.log("categg1= "+categg1);
 console.log("selected_cat= "+selected_cat);
 console.log(document.getElementById("file_cb"+index).value);
-if(catSelection && !checkFlagItem) {
+alert("catSelection = "+catSelection+" checkFlagItem = "+checkFlagItem);
+if(catSelection ) {
 	mainCheckedItems[contentCheckedIndex] = document.getElementById("file_cb"+index).value;
 	contentCheckedIndex++;
 }
@@ -2203,7 +2204,7 @@ checkFlagItem = true;
 //alert("mainCheckedItems"+mainCheckedItems[contentCheckedIndex -1]);
 //alert("mainUncheckItems"+mainUncheckItems[contentUnCheckedIndex-1]);
 }
-if(catSelection && !checkFlagItem) {
+if(catSelection ) {
 		mainUncheckItems[contentUnCheckedIndex] = document.getElementById("file_cb"+index).value;
 		contentUnCheckedIndex++;
 }
