2c05c62d233fa238be44f65a06ac7f045fd7e99b
Ripul Thakur <ripul.thakur@accenture.com>
2013-05-15 18:48:41 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -2594,7 +2594,7 @@ if(catIndex < checkedItemsArray.length) {
 				for(index =0;index < dotIndex;index++) 
 					str = str +'.';
 					dotIndex++;
-				if(dotIndex == 3) dotIndex = 0;
+				if(dotIndex == 4) dotIndex = 0;
 				document.getElementById("frame1").contentDocument.body.innerHTML = "Updating Categories in Progress.<br>Please leave this window open until the moving process has been completed.<br><br><span id='mySpan' style='font-weight:bold;'>"+str.fontcolor("#3778C7")+"</span>";
 
 				
@@ -2673,7 +2673,7 @@ if(catIndex < uncheckItemArray.length) {
 				for(index =0;index < dotIndex;index++) 
 					str = str +'.';
 					dotIndex++;
-				if(dotIndex == 3) dotIndex = 0;
+				if(dotIndex == 4) dotIndex = 0;
 				document.getElementById("frame1").contentDocument.body.innerHTML = "Updating Categories in Progress.<br>Please leave this window open until the moving process has been completed.<br><br><span id='mySpan' style='font-weight:bold;'>"+str.fontcolor("#3778C7")+"</span>";
 				//toUpdateCategories = toUpdateCategories+','+selected_cat;
 				//toUpdateCategories = ["cat1","cat2","cat3"];
@@ -3178,7 +3178,7 @@ else {
 }
 	
 	
-alert('asd');
+
 //**********************End of the code for tags*******************
 function getFiles(space_url)
 {
