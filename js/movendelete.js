b85055df1b4b05beba884bd0419fe8f276eb90c9
Ripul Thakur <ripul.thakur@accenture.com>
2013-05-17 09:22:19 +0530

    commit changes

--- js/movendelete.js
+++ js/movendelete.js
@@ -138,8 +138,8 @@ document.getElementById("frame1").contentDocument.body.style.fontSize = "12px";
 document.getElementById("frame1").contentDocument.body.style.color='Grey';
 document.getElementById("frame1").contentDocument.body.innerHTML = str+"in Progress.<br>Please leave this window open until the "+str+"process has been completed.<br><br><span id='mySpan' style='font-weight:bold;'>"+str2.fontcolor("#3778C7")+"</span>";	
 }
-
-movenContents();
+alert("totalContentSelfUrlArray.length = "+totalContentSelfUrlArray.length);
+//movenContents();
 /*if(discussionSplitValue.length > 1) {
 var str='';
 var str2='';
